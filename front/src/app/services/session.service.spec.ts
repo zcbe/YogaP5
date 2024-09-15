45b7283ab0a77cc1b5f7581545b69aae7ac3ec2f
zcbe <benyahia.zaim@gmail.com>
2024-09-15 11:15:03 +0200

    tests: app/services/session.service

--- front/src/app/services/session.service.spec.ts
+++ front/src/app/services/session.service.spec.ts
@@ -1,7 +1,10 @@
 import { TestBed } from '@angular/core/testing';
 import { expect } from '@jest/globals';
+import { SessionInformation } from '../interfaces/sessionInformation.interface';
 
 import { SessionService } from './session.service';
+import { lastValueFrom } from 'rxjs';
+
 
 describe('SessionService', () => {
   let service: SessionService;
@@ -12,6 +15,40 @@ describe('SessionService', () => {
   });
 
   it('should be created', () => {
+        // THEN
+
     expect(service).toBeTruthy();
   });
+
+  it('should logout', () => {
+
+    // WHEN
+    service.logOut();
+
+    // THEN
+    expect(service.isLogged).toBe(false);
+    expect(service.sessionInformation).toBe(undefined);
+    expect(lastValueFrom(service.$isLogged())).resolves.toBe(false);
+  });
+
+  it('should login', () => {
+    // GIVEN
+    const sessionInfo = {
+      admin: true,
+      token: 'token',
+      id: 1,
+      username: 'username',
+      firstName: 'firstName',
+      lastName: 'lastName',
+      type: 'type',
+    };
+
+    // WHEN
+    service.logIn(sessionInfo);
+
+    // THEN
+    expect(service.isLogged).toBe(true);
+    expect(service.sessionInformation).toEqual(sessionInfo);
+    expect(lastValueFrom(service.$isLogged())).resolves.toBe(true);
+});
 });
