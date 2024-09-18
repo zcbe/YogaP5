0a9f5698ba7b171464fcd000c04eb13c930ddf5e
zcbe <benyahia.zaim@gmail.com>
2024-09-18 19:51:53 +0200

    add Readme

--- front/cypress.config.ts
+++ front/cypress.config.ts
@@ -1,17 +1,26 @@
-import { defineConfig } from 'cypress'
+import { defineConfig } from "cypress";
 
 export default defineConfig({
-  projectId: 'jvf4tw',
-  videosFolder: 'cypress/videos',
-  screenshotsFolder: 'cypress/screenshots',
-  fixturesFolder: 'cypress/fixtures',
+  projectId: "jvf4tw",
+  videosFolder: "cypress/videos",
+  screenshotsFolder: "cypress/screenshots",
+  fixturesFolder: "cypress/fixtures",
   video: false,
+
   e2e: {
     // We've imported your old cypress plugins here.
     // You may want to clean this up later by importing these.
     setupNodeEvents(on, config) {
-      return require('./cypress/plugins/index.ts').default(on, config)
+      return require("./cypress/plugins/index.ts").default(on, config);
     },
-    baseUrl: 'http://localhost:4200',
+    baseUrl: "http://localhost:4200",
   },
-})
+
+  component: {
+    devServer: {
+      framework: "angular",
+      bundler: "webpack",
+    },
+    specPattern: "**/*.cy.ts",
+  },
+});
